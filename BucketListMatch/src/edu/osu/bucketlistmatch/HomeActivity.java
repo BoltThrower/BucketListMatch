90527324542bbd4fb1266a6ee9cccdddb0238a10
Shi Ho Wang <wang.shiho@gmail.com>
2013-02-20 18:06:26 -0500

    Added drop-down menu. It's not fully functional yet.

--- BucketListMatch/src/edu/osu/bucketlistmatch/HomeActivity.java
+++ BucketListMatch/src/edu/osu/bucketlistmatch/HomeActivity.java
@@ -1,16 +1,67 @@
 package edu.osu.bucketlistmatch;
 
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
 
-public class HomeActivity extends SherlockActivity {
+public class HomeActivity extends SherlockFragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		
+		// Create an instance of HomeFragment
+        HomeFragment homeFrag = new HomeFragment();
+
+        // In case this activity was started with special instructions from an Intent,
+        // pass the Intent's extras to the fragment as arguments
+        homeFrag.setArguments(getIntent().getExtras());
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, homeFrag).commit();
+		
+        
+		// Provides data for the spinner widget.
+		SpinnerAdapter spinnerAdapter = ArrayAdapter.createFromResource(this,
+				R.array.action_list,
+				android.R.layout.simple_dropdown_item_1line);
+		
+		// Allows drop-down menu in actionbar.
+		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+		// Navigation listener.
+		OnNavigationListener navLis = new OnNavigationListener() {
+			// Get the same strings provided for the drop-down's ArrayAdapter
+			String[] strings = getResources().getStringArray(
+					R.array.action_list);
+
+			@Override
+			public boolean onNavigationItemSelected(int position, long itemId) {
+				// Some how create the appropriate fragment to replace with.
+				// Create new fragment from our own Fragment class
+				HomeFragment frag = new HomeFragment();
+
+				// Replace whatever is in the fragment container with this
+				// fragment
+				// and give the fragment a tag name equal to the string at the
+				// position selected
+				getSupportFragmentManager().beginTransaction()
+						.replace(R.id.fragment_container, frag,
+								strings[position]).commit();
+				return true;
+			}
+		};
+
+		// Set items and navigation listener to the actionbar.
+		getSupportActionBar().setListNavigationCallbacks(spinnerAdapter, navLis);
 	}
 
 	@Override
@@ -19,5 +70,4 @@ public class HomeActivity extends SherlockActivity {
 		getSupportMenuInflater().inflate(R.menu.home, menu);
 		return true;
 	}
-
 }
---
+++ BucketListMatch/src/edu/osu/bucketlistmatch/HomeFragment.java
@@ -0,0 +1,32 @@
+package edu.osu.bucketlistmatch;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class HomeFragment extends SherlockFragment{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+	        Bundle savedInstanceState) {
+		
+		
+		return inflater.inflate(R.layout.activity_splash_screen, container, false);
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		
+	}
+}
