dfd4b3a50162ad6bc86a731fb955dcf55c100102
Chris <Chris@75.187.63.112>
2013-02-27 21:48:27 -0500

    Implemented functionality to take input of a username and password from the login activity class.

--- BucketListMatch/src/edu/osu/bucketlistmatch/LoginActivity.java
+++ BucketListMatch/src/edu/osu/bucketlistmatch/LoginActivity.java
@@ -3,16 +3,34 @@ package edu.osu.bucketlistmatch;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 
+import database.DB;
+
+import android.R.string;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Context;
 import android.content.Intent;
 
 public class LoginActivity extends SherlockActivity {
+	
+	EditText username, password;
+	Button signin;
+	String user, pass;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		username = (EditText)findViewById(R.id.username); 
+		password = (EditText)findViewById(R.id.password);
+		signin = (Button)findViewById(R.id.signInButton);
+		
+		signin.setOnClickListener(signinListener);
 	}
 
 	@Override
@@ -26,5 +44,38 @@ public class LoginActivity extends SherlockActivity {
 		Intent i = new Intent(this, HomeActivity.class);
 		startActivity(i);
 	}
+	
+	
+	private OnClickListener signinListener = new OnClickListener(){
+		
+		public void onClick (View v)
+		{
+			// Authenticate the login information.
+			//System.out.println(DB.validateUser(user, pass));
+			//int result = query.validateUser(user, pass);
+			//System.out.println(result);
+			user = username.getText().toString(); 
+			pass = password.getText().toString();
+			
+			// Notification for a bad username or password
+			//Toast.makeText(getApplicationContext(), user, Toast.LENGTH_SHORT).show();
+		}
+	};
+	
+	private void authenticateLogin(View view) {
+		
+		//int result = query.validateUser(user, pass);
+		//System.out.println(result);
+		//System.out.println(user);
+		//System.out.println(pass);
+		//Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+		
+		//Intent i = new Intent(this, HomeActivity.class);
+		//startActivity(i);
+		
+		
+	}
+	
+
 
 }
--- BucketListMatch/src/edu/osu/bucketlistmatch/MainActivity.java
+++ BucketListMatch/src/edu/osu/bucketlistmatch/MainActivity.java
@@ -1,18 +1,32 @@
 package edu.osu.bucketlistmatch;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
+
+import database.DB;
 
 import android.os.Bundle;
 import android.content.Intent;
 
 public class MainActivity extends SherlockActivity {
 
+	public DB db;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Establish database connection to application
+		//db = new DB(DatabaseSetup());
+
+		// Close the connection to database to release resources
+		//db.close();
 		
+		// Start splash screen activity
 		Intent intent = new Intent(this, SplashScreenActivity.class);
 		startActivity(intent);
 	}
@@ -22,6 +36,32 @@ public class MainActivity extends SherlockActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	static Connection DatabaseSetup() {
+		try {
+
+			// Fetch the class to be used in the connection
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+			// Define the Java DB connectivity URL
+			String connectionUrl = "jdbc:sqlserver://server14.ies.cse.ohio-state.edu:1433;"
+					+ "databaseName={BLM_Database};userName=ApplAccount;password=@pplBLM!cc0uNt59i1;";
+
+			// Set up a connection using Java's driver manager
+			return DriverManager.getConnection(connectionUrl);
+
+		} catch (ClassNotFoundException e) {
+			// TODO Handle exception
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Handle exception
+			e.printStackTrace();
+		}
+		return null; // TODO If code gets to this point (i.e. database is not
+						// connected),
+						// show error message?
+
 	}
 
 }
