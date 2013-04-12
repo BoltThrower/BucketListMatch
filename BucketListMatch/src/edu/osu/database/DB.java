8c4dfbb07fe53b998ce719dfd6ab5124cc644359
Everly <evra4iec@gmail.com>
2013-04-12 01:24:24 -0400

    I edited the functions in the DB class for efficiency and documentation purposes. They can now be called by the application (also call those that haven't been implemented yet; they return -1).
    I also noticed the BucketList still doesn't display anything. The
    getAllBucketListBooks function is ready to be called.

--- BucketListMatch/src/edu/osu/database/DB.java
+++ BucketListMatch/src/edu/osu/database/DB.java
@@ -19,10 +19,6 @@ public class DB {
 	private static JSONParser parser;
 	private final static String URL_main = "http://server14.ies.cse.ohio-state.edu/BLM/";
 
-	public static enum User {
-		password, profilePicture, firstname, lastname, description, dateOfBirth, city, state, country, zipCode, email, phone
-	};
-
 	/*
 	 * NOTE: Each method checks that the entries are valid before passing them
 	 * on to the database.
@@ -98,6 +94,60 @@ public class DB {
 
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public static JSONArray getPublicDreamBooks() {
+		JSONArray result = null;
+
+		try {
+			parser = new JSONParser(URL_main
+					+ "dreambook/fetchAllDreamBooks.php");
+			result = parser.getJSONArray();
+		} catch (JSONException e) {
+			Log.e("JSONParser Error.",
+					"Result of JSON Array may be null, or contain a null value being referenced. Error occurred in fetchBucketListBooks.");
+		}
+
+		return result;
+	}
+
+	public static JSONArray getAllBucketListBooks(String username,
+			String password) {
+		JSONArray result = null;
+
+		try {
+			parser = new JSONParser(URL_main
+					+ "dreambook/fetchBucketListBooks.php?u="
+					+ Helper.parseForHTTP(username) + "&p="
+					+ Helper.parseForHTTP(password));
+			result = parser.getJSONArray();
+		} catch (JSONException e) {
+			Log.e("JSONParser Error.",
+					"Result of JSON Array may be null, or contain a null value being referenced. Error occurred in fetchScrapbooks.");
+		}
+
+		return result;
+	}
+
+	public static JSONArray getAllScrapbooks(String username, String password) {
+		JSONArray result = null;
+
+		try {
+			parser = new JSONParser(URL_main
+					+ "dreambook/fetchScrapbooks.php?u="
+					+ Helper.parseForHTTP(username) + "&p="
+					+ Helper.parseForHTTP(password));
+			result = parser.getJSONArray();
+		} catch (JSONException e) {
+			Log.e("JSONParser Error.",
+					"Result of JSON Array may be null, or contain a null value being referenced. Error occurred in fetchScrapbooks.");
+		}
+
+		return result;
+	}
+
 	public static JSONArray match(String username, String password) {
 		JSONArray result = null;
 
@@ -116,7 +166,6 @@ public class DB {
 
 	// UPDATE METHODS
 
-	// TODO Implement
 	/**
 	 * This method takes user information through a string and adds the user to
 	 * the database.
@@ -141,8 +190,8 @@ public class DB {
 					+ Helper.parseForHTTP(user[4]) + "&co="
 					+ Helper.parseForHTTP(user[5]) + "&e="
 					+ Helper.parseForHTTP(user[6]));
-			JSONArray tmp = parser.getJSONArray();
-			if (tmp.length() != 0) return 1;
+			if (parser.getJSONArray() == null)
+				return 2;
 		} catch (JSONException e) {
 			Log.e("JSONParser Error.",
 					"Result of JSON Array may be null, or contain a null value being referenced. Error occurred in addUser.");
@@ -166,16 +215,91 @@ public class DB {
 	 *            the new photo to be used.
 	 * @return
 	 */
-	static int changeUserInfo(int type, String username, String password,
-			String info) {
-		return -1;
+	public static int changeUserInfo(int type, String username,
+			String password, String info) {
+
+		username = Helper.parseForHTTP(username);
+		password = Helper.parseForHTTP(password);
+		info = Helper.parseForHTTP(info);
+
+		try {
+			switch (type) {
+			case Enum.C_PASS:
+				parser = new JSONParser(URL_main + "user/changePassword.php?u="
+						+ username + "&p=" + password + "&np=" + info);
+				break;
+			case Enum.C_PROFILE_PIC:
+				parser = new JSONParser(URL_main + "user/changePhoto.php?u="
+						+ username + "&p=" + password + "&pp=" + info);
+				break;
+			case Enum.C_FIRST:
+				parser = new JSONParser(URL_main
+						+ "user/changeFirstName.php?u=" + username + "&p="
+						+ password + "&fn=" + info);
+				break;
+			case Enum.C_LAST:
+				parser = new JSONParser(URL_main + "user/changeLastName.php?u="
+						+ username + "&p=" + password + "&ln=" + info);
+				break;
+			case Enum.C_DESCR:
+				parser = new JSONParser(URL_main
+						+ "user/changeDescription.php?u=" + username + "&p="
+						+ password + "&d=" + info);
+				break;
+			case Enum.C_DOB:
+				parser = new JSONParser(URL_main
+						+ "user/changeDateOfBirth.php?u=" + username + "&p="
+						+ password + "&db=" + info);
+				break;
+			case Enum.C_CITY:
+				parser = new JSONParser(URL_main + "user/changeCity.php?u="
+						+ username + "&p=" + password + "&c=" + info);
+				break;
+			case Enum.C_STATE:
+				parser = new JSONParser(URL_main + "user/changeState.php?u="
+						+ username + "&p=" + password + "&s=" + info);
+				break;
+			case Enum.C_COUNTRY:
+				parser = new JSONParser(URL_main + "user/changeCountry.php?u="
+						+ username + "&p=" + password + "&co=" + info);
+				break;
+			case Enum.C_ZIP:
+				parser = new JSONParser(URL_main + "user/changeZipCode.php?u="
+						+ username + "&p=" + password + "&z=" + info);
+				break;
+			case Enum.C_EMAIL:
+				parser = new JSONParser(URL_main + "user/changeEmail.php?u="
+						+ username + "&p=" + password + "&e=" + info);
+				break;
+			case Enum.C_PHONE:
+				parser = new JSONParser(URL_main + "user/changePhone.php?u="
+						+ username + "&p=" + password + "&ph=" + info);
+				break;
+			default:
+				return 3;
+			}
+
+			if (parser.getJSONArray() == null)
+				return 2;
+		} catch (JSONException e) {
+			return 1;
+		}
+		return 0;
 	}
 
-	static int deleteUser(String username, String password) {
-		return -1;
+	public static int deleteUser(String username, String password) {
+		try {
+			parser = new JSONParser(URL_main + "user/deleteUser.php?u="
+					+ Helper.parseForHTTP(username) + "&p="
+					+ Helper.parseForHTTP(password));
+			if (parser.getJSONArray() == null)
+				return 2;
+		} catch (JSONException e) {
+			return 1;
+		}
+		return 0;
 	}
 
-	// TODO Implement
 	/**
 	 * This method validates user credentials, then adds a bucketlist entry to
 	 * the database.
@@ -187,106 +311,268 @@ public class DB {
 	 * @return int 0 if successful, otherwise a status number is returned
 	 *         depending on the error.
 	 */
-	static int addBucketListBook(String username, String password,
+	public static int addBucketListBook(String username, String password,
 			String[] book, boolean privacy) {
-		return -1;
-
-	}
-
-	/**
-	 * 
-	 * @return
-	 */
-	public static JSONArray getPublicDreamBooks() {
-		JSONArray result = null;
-
+		String privacy_char;
+		if (privacy)
+			privacy_char = "Y";
+		else
+			privacy_char = "N";
 		try {
 			parser = new JSONParser(URL_main
-					+ "dreambook/fetchAllDreamBooks.php");
-			result = parser.getJSONArray();
+					+ "dreambook/addBucketListBook.php?u="
+					+ Helper.parseForHTTP(username) + "&p="
+					+ Helper.parseForHTTP(password) + "&n="
+					+ Helper.parseForHTTP(book[0]) + "&i="
+					+ Helper.parseForHTTP(book[1]) + "&s="
+					+ Helper.parseForHTTP(book[2]) + "&co="
+					+ Helper.parseForHTTP(book[3]) + "&sd="
+					+ Helper.parseForHTTP(book[4]) + "&pr=" + privacy_char);
+			if (parser.getJSONArray() == null)
+				return 2;
 		} catch (JSONException e) {
-			Log.e("JSONParser Error.",
-					"Result of JSON Array may be null, or contain a null value being referenced. Error occurred in fetchBucketListBooks.");
+			return 1;
 		}
+		return 0;
 
-		return result;
 	}
 
-	static int editBucketListBookPrivacy(String username, String password,
-			String bookName, boolean new_privacy) {
-		return -1;
+	public static int editBucketListBookPrivacy(String username,
+			String password, String bookName, boolean new_privacy) {
+		try {
+			if (new_privacy == true) {
+				parser = new JSONParser(URL_main
+						+ "dreambook/setPrivate.php?u="
+						+ Helper.parseForHTTP(username) + "&p="
+						+ Helper.parseForHTTP(password) + "&n="
+						+ Helper.parseForHTTP(bookName));
+			} else {
+				parser = new JSONParser(URL_main + "dreambook/setPublic.php?u="
+						+ Helper.parseForHTTP(username) + "&p="
+						+ Helper.parseForHTTP(password) + "&n="
+						+ Helper.parseForHTTP(bookName));
+			}
+			if (parser.getJSONArray() == null)
+				return 2;
+		} catch (JSONException e) {
+			return 1;
+		}
+		return 0;
 	}
 
-	static int editBucketListBookInfo(String username, String password,
-			String bookName, String info) {
-		return -1;
-	}
+	public static int editBucketListBookInfo(int type, String username,
+			String password, String bookName, String info) {
 
-	public static JSONArray getAllScrapbooks(String username, String password) {
-		JSONArray result = null;
+		username = Helper.parseForHTTP(username);
+		password = Helper.parseForHTTP(password);
+		bookName = Helper.parseForHTTP(bookName);
+		info = Helper.parseForHTTP(info);
 
 		try {
-			parser = new JSONParser(URL_main + "dreambook/fetchScrapbooks.php?u=" + Helper.parseForHTTP(username) + "&p=" + Helper.parseForHTTP(password));
-			result = parser.getJSONArray();
+			switch (type) {
+			case Enum.B_IMAGE:
+				parser = new JSONParser(URL_main + "dreambook/editImage.php?u="
+						+ username + "&p=" + password + "&n=" + bookName
+						+ "&i=" + info);
+				break;
+			case Enum.B_STATE:
+				parser = new JSONParser(URL_main + "dreambook/editState.php?u="
+						+ username + "&p=" + password + "&n=" + bookName
+						+ "&s=" + info);
+				break;
+			case Enum.B_COUNTRY:
+				parser = new JSONParser(URL_main
+						+ "dreambook/editCountry.php?u=" + username + "&p="
+						+ password + "&n=" + bookName + "&co=" + info);
+				break;
+			case Enum.B_STARTDATE:
+				parser = new JSONParser(URL_main
+						+ "dreambook/editStartDate.php?u=" + username + "&p="
+						+ password + "&n=" + bookName + "&sd=" + info);
+				break;
+			default:
+				return 3;
+			}
+
+			if (parser.getJSONArray() == null)
+				return 2;
 		} catch (JSONException e) {
-			Log.e("JSONParser Error.",
-					"Result of JSON Array may be null, or contain a null value being referenced. Error occurred in fetchScrapbooks.");
+			return 1;
 		}
 
-		return result;
+		return 0;
 	}
 
-	public static JSONArray getAllBucketListBooks(String username, String password) {
-		JSONArray result = null;
-
+	public static int moveToScrapbook(String username, String password,
+			String bookName) {
 		try {
-			parser = new JSONParser(URL_main + "dreambook/fetchBucketListBooks.php?u=" + Helper.parseForHTTP(username) + "&p=" + Helper.parseForHTTP(password));
-			result = parser.getJSONArray();
+			parser = new JSONParser(URL_main
+					+ "dreambook/moveToScrapbook.php?u="
+					+ Helper.parseForHTTP(username) + "&p="
+					+ Helper.parseForHTTP(password) + "&n="
+					+ Helper.parseForHTTP(bookName));
+			if (parser.getJSONArray() == null)
+				return 2;
 		} catch (JSONException e) {
-			Log.e("JSONParser Error.",
-					"Result of JSON Array may be null, or contain a null value being referenced. Error occurred in fetchScrapbooks.");
+			return 1;
 		}
-
-		return result;
+		return 0;
 	}
-	
-	static int moveToScrapbook(String username, String password, String bookName) {
+
+	public static int deleteDreamBook(String username, String password,
+			String bookName) {
+		try {
+			parser = new JSONParser(URL_main
+					+ "dreambook/deleteDreamBook.php?u="
+					+ Helper.parseForHTTP(username) + "&p="
+					+ Helper.parseForHTTP(password) + "&n="
+					+ Helper.parseForHTTP(bookName));
+			if (parser.getJSONArray() == null)
+				return 2;
+		} catch (JSONException e) {
+			return 1;
+		}
+		return 0;
+	}
+
+	public static int addChapter(String username, String password,
+			String bookName, String[] chapterInfo) {
+		try {
+			parser = new JSONParser(URL_main + "chapter/addChapter.php?u="
+					+ Helper.parseForHTTP(username) + "&p="
+					+ Helper.parseForHTTP(password) + "&n="
+					+ Helper.parseForHTTP(bookName) + "&ch="
+					+ Helper.parseForHTTP(chapterInfo[0]) + "&sd="
+					+ Helper.parseForHTTP(chapterInfo[1]) + "&du="
+					+ Helper.parseForHTTP(chapterInfo[2]) + "&co="
+					+ Helper.parseForHTTP(chapterInfo[3]) + "&ur="
+					+ Helper.parseForHTTP(chapterInfo[4]) + "&rom="
+					+ Helper.parseForHTTP(chapterInfo[5]) + "&des="
+					+ Helper.parseForHTTP(chapterInfo[6]) + "&pr="
+					+ Helper.parseForHTTP(chapterInfo[7]));
+			if (parser.getJSONArray() == null)
+				return 2;
+		} catch (JSONException e) {
+			return 1;
+		}
+		return 0;
+	}
+
+	// TODO
+	public static int addChapterToBook(String username, String password,
+			String bookName, String chapterName, String chapterCreator) {
 		return -1;
 	}
 
-	static int deleteDreamBook(String username, String password,
-			String bookName, boolean type) {
+	// TODO
+	public static int editChapterInfo(int type, String username,
+			String password, String chapterName, String info) {
 		return -1;
 	}
 
-	static int addChapter(String username, String password, String bookName,
-			String[] chapterInfo) {
-		return -1;
-	}
-
-	static JSONArray getAllChapters(String bookName) {
-		JSONArray result = null;
-
-		return result;
-	}
-
-	static int removeChapterFromBook(String username, String password,
-			String bookName, String chapterName) {
-		return -1;
-	}
-
-	static int addMediaToChapter(String username, String password,
+	// TODO
+	public static int addMediaToChapter(String username, String password,
 			String bookName, String chapterName, String[] mediaInfo) {
 		return -1;
 	}
 
-	static int addChallengeToChapter(String username, String password,
+	// TODO
+	public static int addTotemToChapter(String username, String password,
+			String bookName, String chapterName, String[] totemInfo) {
+		return -1;
+	}
+
+	// TODO
+	public static int addChallengeToChapter(String username, String password,
 			String bookName, String chapterName, String[] challengeInfo) {
 		return -1;
 	}
 
-	static int addTotemToChapter(String username, String password,
-			String bookName, String chapterName, String[] totemInfo) {
+	// TODO
+	public static int editMediaInfo(int type, String username, String password,
+			String chapterName, String info) {
+		return -1;
+	}
+
+	// TODO
+	public static int editTotemInfo(int type, String username, String password,
+			String chapterName, String info) {
+		return -1;
+	}
+
+	// TODO
+	public static int editChallengeInfo(int type, String username,
+			String password, String chapterName, String info) {
+		return -1;
+	}
+
+	// TODO
+	public static int removeMediaFromChapter(String username, String password,
+			String chapterName, String mediaName) {
+		return -1;
+	}
+
+	// TODO
+	public static int removeTotemFromChapter(String username, String password,
+			String chapterName, String TotemName) {
+		return -1;
+	}
+
+	// TODO
+	public static int removeChallengeFromChapter(String username,
+			String password, String chapterName, String challengeName) {
+		return -1;
+	}
+
+	// TODO
+	public static int removeChapterFromBook(String username, String password,
+			String bookName, String chapterName) {
+		return -1;
+	}
+
+	// TODO
+	public static int deleteChapter(String username, String password,
+			String chapterName) {
+		return -1;
+	}
+
+	// TODO
+	public static int addDeal(String username, String password,
+			String[] dealInfo) {
+		return -1;
+	}
+
+	// TODO
+	public static int editDealInfo(int type, String username, String password,
+			String dealName, String info) {
+		return -1;
+	}
+
+	// TODO
+	public static int deleteDeal(String username, String password,
+			String dealName) {
+		return -1;
+	}
+
+	// TODO
+	public static int addMerchant(String[] merchantInfo) {
+		return -1;
+	}
+
+	// TODO
+	public static int addMerchantReview(String username, String password,
+			String merchantName, String description, int rating) {
+		return -1;
+	}
+
+	// TODO
+	public static int editMerchantInfo(int type, String username,
+			String password, String info) {
+		return -1;
+	}
+
+	// TODO
+	public static int deleteMerchant(String username, String password) {
 		return -1;
 	}
 
--- BucketListMatch/src/edu/osu/database/Enum.java
+++ BucketListMatch/src/edu/osu/database/Enum.java
@@ -2,13 +2,10 @@ package edu.osu.database;
 
 public class Enum {
 
-	static final int CUSTOMER_PROFILE_PIC_INDEX = 3;
+	public static final int C_PASS = 0, C_PROFILE_PIC = 1, C_FIRST = 2, C_LAST = 3;
+	public static final int C_DESCR = 4, C_DOB = 5, C_CITY = 6, C_STATE = 7, C_COUNTRY = 8;
+	public static final int C_ZIP = 9, C_EMAIL = 10, C_PHONE = 11;
 	
-	public static final int CUSTOMER_LENGTH = 13;
-	public static final int C_USERNAME = 1, C_PASS = 2, C_PROFILE_PIC = 3, C_FIRST = 4, C_LAST = 5;
-	public static final int C_DESCR = 6, C_DOB = 7, C_CITY = 8, C_STATE = 9, C_COUNTRY = 10;
-	public static final int C_ZIP = 11, C_EMAIL = 12, C_PHONE = 13;
+	public static final int B_IMAGE = 0, B_STATE = 1, B_COUNTRY = 2, B_STARTDATE = 3;
 	
-	public static final int UPDATE_PHOTO = 0, UPDATE_LOCATION = 1, UPDATE_DESCRIPTION = 2,
-			UPDATE_ZIPCODE = 3, UPDATE_PHONE = 4;
 }
