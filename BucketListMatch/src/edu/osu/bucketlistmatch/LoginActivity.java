cf48a0bbf4904c50918e7278bf765bc5a0e100da
Shi Ho Wang <wang.shiho@gmail.com>
2013-04-13 16:59:32 -0400

    Added forget password view.

--- BucketListMatch/src/edu/osu/bucketlistmatch/LoginActivity.java
+++ BucketListMatch/src/edu/osu/bucketlistmatch/LoginActivity.java
@@ -16,13 +16,14 @@ import android.content.Context;
 import android.content.Intent;
 
 public class LoginActivity extends SherlockActivity {
-/**
- * This activity allows the user to request for an e-mail for a temporary password to log back in.
- * 
- * @author Chris Hartman
- *
- */
-	
+	/**
+	 * This activity allows the user to request for an e-mail for a temporary
+	 * password to log back in.
+	 * 
+	 * @author Chris Hartman
+	 * 
+	 */
+
 	EditText username, password;
 	Button signin;
 	static String user, pass;
@@ -31,60 +32,58 @@ public class LoginActivity extends SherlockActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		
-		username = (EditText)findViewById(R.id.username); 
-		password = (EditText)findViewById(R.id.password);
-		signin = (Button)findViewById(R.id.signInButton);
-		
+
+		username = (EditText) findViewById(R.id.username);
+		password = (EditText) findViewById(R.id.password);
+		signin = (Button) findViewById(R.id.signInButton);
+
 		signin.setOnClickListener(signinListener);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		//getSupportMenuInflater().inflate(R.menu.login, menu);
+		// getSupportMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-	
+
 	public void openHome(View view) {
 		Intent i = new Intent(this, HomeActivity.class);
 		startActivity(i);
 	}
-	
+
 	/**
 	 * Opens the sign up page.
+	 * 
 	 * @param v
 	 */
-	public void openSignUp(View v) {
-		Intent i = new Intent(this, SignUpActivity.class);
-		startActivity(i);
+	public void onClick(View view) {
+
+		switch (view.getId()) {
+		case R.id.forgetPasswordButton:
+			startActivity(new Intent(this, ForgetPasswordActivity.class));
+			break;
+		case R.id.signUpButton:
+			startActivity(new Intent(this, SignUpActivity.class));
+			break;
+		}
 	}
-	
-	public void forgetPassword(View v){
-		//Intent i = new Intent(this, ForgetPasswordActivity.class);
-		//startActivity(i);
-	}
-	
-	private OnClickListener signinListener = new OnClickListener(){
-		
-		public void onClick (View v)
-		{
-			user = username.getText().toString(); 
+
+	private OnClickListener signinListener = new OnClickListener() {
+
+		public void onClick(View v) {
+			user = username.getText().toString();
 			pass = password.getText().toString();
 
 			// Authenticate the login information.
 			int status = DB.validateUser(user, pass);
-			
-			if (status == 0) openHome(v);
-			else Toast.makeText(getApplicationContext(), "Authentication Failed. Try again.", Toast.LENGTH_SHORT).show();
+
+			if (status == 0)
+				openHome(v);
+			else
+				Toast.makeText(getApplicationContext(),
+						"Authentication Failed. Try again.", Toast.LENGTH_SHORT)
+						.show();
 		}
 	};
-	
-	public String getUser() {
-		return user;
-	}
-	
-	public String getPass() {
-		return pass;
-	}
 }
